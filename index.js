a5110cd0f3104a8abcda2263631afdd3f168a2d2
IMH51 <ianmartinharrison@outlook.com>
2019-01-07 22:09:10 +0000

    Completed js filter and map lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (driver) { return driver.revenue > revenue})
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function (driver) {return driver.name})
+}
+
+function exactMatch(drivers, attrObject) {
+  let key = Object.keys(attrObject)
+  let value = Object.values(attrObject)
+  return drivers.filter(function (driver) { return driver[key[0]] === value[0]})
+}
+
+function exactMatchToList(drivers, attrObject) {
+  return exactMatch(drivers, attrObject).map(function (driver) { return driver.name })
+}
